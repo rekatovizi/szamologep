0b9b888a396c0254e9ca166c3d80a59f197882a2
TóviziRéka <TviziRka@RRF16E-001722>
2026-09-16 10:43:18 +0200

    Szamologep kinézet

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -19,6 +19,68 @@ namespace szamologep
         public MainWindow()
         {
             InitializeComponent();
+            GombokElhelyezese();
+        }
+
+        private void GombokElhelyezese()
+        {
+            // Gombok elhelyezése a Grid-ben
+            for (int i = 0; i < 4; i++)
+            {
+                ButtonGrid.RowDefinitions.Add(new RowDefinition());
+                ButtonGrid.ColumnDefinitions.Add(new ColumnDefinition());
+                
+            }
+            string[,] feliratok = new string[,]
+            {
+                { "7", "8", "9", "/" },
+                { "4", "5", "6", "*" },
+                { "1", "2", "3", "-" },
+                { "C", "0", "=", "+" }
+            };  
+
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    string label = feliratok[i, j];
+                    Button btn = new Button
+                    {
+                        Content = label,
+                        FontSize = 20,
+                        FontWeight = FontWeights.Bold,
+                        Margin = new Thickness(3)
+                    };
+                    if (char.IsDigit(label[0]))
+                    {
+                        btn.Background = Brushes.WhiteSmoke;
+                    }
+                    else if (label == "C")
+                    {
+                        btn.Background = Brushes.IndianRed;
+                        btn.Foreground = Brushes.White;
+                    }
+                    else
+                    {
+                        btn.Background = Brushes.DodgerBlue;
+                        btn.Foreground = Brushes.White;
+                    }
+
+                    btn.Click += Button_Click;
+
+                    Grid.SetRow(btn, i);
+                    Grid.SetColumn(btn, j);
+                    ButtonGrid.Children.Add(btn);
+                }
+            }
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = (Button)sender;
+            string felirat = button.Content.ToString();
+            tb_kijelzo.Text += felirat;
         }
     }
 }
